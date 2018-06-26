e351d23c546f71f52f65445d06b90e2151c810b4
zxfjd3g <258147149@qq.com>
2018-06-26 10:57:37 +0800

    聊天后台

--- socketIO/server_socket.js
+++ socketIO/server_socket.js
@@ -4,10 +4,10 @@ module.exports = function (server) {
   const io = require('socket.io')(server)
   // 监视与浏览器连接, 接收到一个连接对象(socket)
   io.on('connection', function (socket) {// 与某个浏览器的连接
-
+    console.log('有一个客户端连接上了')
     // 监视当前socket对应浏览器向服务器发送消息
     socket.on('sendMsg', function ({from, to, content}) { // 浏览器发送的消息数据(data)
-
+      console.log('接收到一个聊天消息: ', from, to, content)
       // 1. 保存chat到chats集合中
       const chat = {
         from,
@@ -19,6 +19,7 @@ module.exports = function (server) {
       new ChatModel(chat).save(function (error, chatMsg) {
         // 2. 向浏览器发送消息(chat对象)
         io.emit('receiveMsg', chatMsg)
+        console.log('向浏览器端分发保存的聊天消息', chatMsg)
       })
     })
   })
